ee63184d5d2b1ef7404147df4bbd1f31a77e7b2d
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-11 10:56:49 -0500

    Task Done

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VariablesExerciseTC2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string petName = "Ralph";
+
+            int petAge = 10;
+
+            char petNickName = 'R';
+
+            bool petHasLegs = true;
+
+            double yearBorn = 2008;
+
+            decimal petWeight = 25.7m;
+
+            Console.WriteLine($"My dog's name is {petName}, he's {petAge} years old.");
+            Console.WriteLine($"{petName} is called {petNickName} most times and its {petHasLegs} he has four legs.");
+            Console.WriteLine($"{petName} was born in {yearBorn} and weighs {petWeight} pounds.");
+          
+
+        }
+    }
+}
